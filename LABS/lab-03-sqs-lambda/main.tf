f503fcd21620cfa777e5738db652b9b90f77363e
Kunchala Ashok <93498307+Ashokkunchala@users.noreply.github.com>
2026-09-20 14:33:34 +0530

    lab 03: add archive provider

--- LABS/lab-03-sqs-lambda/main.tf
+++ LABS/lab-03-sqs-lambda/main.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">= 1.6.0"
-  required_providers { aws = { source = "hashicorp/aws", version = "~> 6.0" } }
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 6.0" }
+    archive = { source = "hashicorp/archive", version = "~> 2.7" }
+  }
 }
 provider "aws" { region = var.region }
 variable "region" { type = string; default = "us-east-1" }
